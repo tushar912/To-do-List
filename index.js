1272023042e0c95f86374636b4b0b79f91876d11
tushar912 <tushar.912u@gmail.com>
2020-04-02 15:41:57 +0530

    final

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ const express=require("express");
 const bodyparser=require("body-parser");
 
 const app=express();
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.set('view engine','ejs');
+var items=[];
 app.get("/",function(req,res){
   var date =new Date();
-  var currentday=date.getDay();
-  if(currentday===6||currentday===0){
-var k="weekend";
 
-}
-else{
-  var k="weekday";
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  }
+  var day =date.toLocaleDateString("en-US",options);
 
-}
-res.render("index",{f:k});
+
+res.render("index",{f:day,items:items});
+})
+app.post("/",function(req,res){
+var body
+  var item=req.body.itam;
+  items.push(item);
+  res.redirect("/");
 })
 app.listen(300,function(){
 console.log("server on");
