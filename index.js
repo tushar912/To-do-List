529abe9cc117f38ae0c07e70062f3a22e99c403d
tushar912 <tushar.912u@gmail.com>
2020-04-03 10:55:44 +0530

    added template

--- index.js
+++ index.js
@@ -6,25 +6,40 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
 var items=[];
+var workitems=[];
+var date =new Date();
+
+var options={
+  weekday:"long",
+  day:"numeric",
+  month:"long"
+};
+var day =date.toLocaleDateString("en-US",options);
+
 app.get("/",function(req,res){
-  var date =new Date();
-
-  var options={
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-  }
-  var day =date.toLocaleDateString("en-US",options);
 
 
-res.render("index",{f:day,items:items});
+res.render("index",{f:day,items:items,v:"list"});
 })
 app.post("/",function(req,res){
-var body
   var item=req.body.itam;
+
+  if(req.body.button==="work"){
+workitems.push(item);
+    res.redirect("/work");
+  }
+  else{
+
   items.push(item);
   res.redirect("/");
+}
 })
+
+app.get("/work",function(req,res){
+  res.render("index",{f:day,items:workitems,v:"work"});
+
+})
+
 app.listen(300,function(){
 console.log("server on");
 })
