660273fa66e053f86acb386962e7ebeecd191d8a
tushar912 <tushar.912u@gmail.com>
2020-04-06 13:56:46 +0530

    added DB

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express=require("express");
 const bodyparser=require("body-parser");
-
+const mongoose=require("mongoose");
 const app=express();
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
-var items=[];
+mongoose.connect("mongodb://localhost:27017/todolistdb",{useUnifiedTopology:true,useNewUrlParser:true});
+const itemsSchema={
+  name:String
+};
+const Item=mongoose.model("Item",itemsSchema);
+
 var workitems=[];
 var date =new Date();
+const item1=new Item({name:"todolist"});
+const item2=new Item({name:"hi"});
+const arr=[item1,item2];
+
+
 
 var options={
   weekday:"long",
@@ -17,29 +27,48 @@ var options={
 var day =date.toLocaleDateString("en-US",options);
 
 app.get("/",function(req,res){
+  Item.find({},function(err,founditems){
+    if(founditems.length==0){
+      Item.insertMany(arr,function(err){
+        if(err){
+          console.log("err");
+        }
+        else{
+          console.log("added success");
+        }
+      })
+        res.redirect("/");
+    }
+    else
+    res.render("index",{f:day,items:founditems,v:"list"});
 
+  })
 
-res.render("index",{f:day,items:items,v:"list"});
+})
+app.post("/delete",function(req,res){
+  const id=req.body.check;
+  Item.findByIdAndRemove(id,function(err){
+    if(err)
+    console.log("err");
+    else
+    console.log("success");
+  })
+  res.redirect("/");
 })
 app.post("/",function(req,res){
-  var item=req.body.itam;
+  const a=req.body.itam;
+const i =new Item({name:a});
 
-  if(req.body.button==="work"){
-workitems.push(item);
-    res.redirect("/work");
-  }
-  else{
-
-  items.push(item);
-  res.redirect("/");
-}
-})
+i.save();
+res.redirect("/");
+  })
 
 app.get("/work",function(req,res){
+
   res.render("index",{f:day,items:workitems,v:"work"});
 
 })
 
-app.listen(300,function(){
+app.listen(3000,function(){
 console.log("server on");
 })
